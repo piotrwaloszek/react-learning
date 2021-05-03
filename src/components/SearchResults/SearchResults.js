e52c8074dc95467b955f0a994875ba489647b16b
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-05-03 18:51:57 +0200

    Add defaultProps for title

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -14,6 +14,7 @@ class SearchResults extends React.Component {
 
   static defaultProps = {
     icon: settings.defaultColumnIcon,
+    title: 'Search Results',
   }
 
   render() {
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ const initialState = {
   lists: initialStoreData.lists,
   columns: initialStoreData.columns,
   cards: initialStoreData.cards,
-  searchString: 'ter',
+  searchString: '',
 };
 
 const reducers = {
